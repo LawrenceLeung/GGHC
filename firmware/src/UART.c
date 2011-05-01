b7f7ec93994768b67a6311a331c705c89a80d188
Ned Konz <ned@bike-nomad.com>
2011-05-01 15:26:21 -0700

    Added more UART code

--- firmware/src/UART.c
+++ firmware/src/UART.c
@@ -3,6 +3,38 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+#define rxBufferSize 128
+#define txBufferSize 128
+
+static volatile uint8_t rxBuffer[rxBufferSize];
+static volatile uint8_t rxBufferOut, rxBufferIn;
+
+static volatile uint8_t txBuffer[txBufferSize];
+static volatile uint8_t txBufferOut, txBufferIn;
+
+void UART5_IRQHandler(void)
+{
+    if(USART_GetITStatus(UART5, USART_IT_RXNE) != RESET)
+    {
+        /* Read one byte from the receive data register */
+        uint8_t c = USART_ReceiveData(UART5);
+
+        if(rxBufferIn < rxBufferSize)
+        {
+            rxBuffer[rxBufferIn++] = c;
+        }
+    }
+
+    if(USART_GetITStatus(UART5, USART_IT_TXE) != RESET)
+    {
+        if (txBufferOut < txBufferIn)
+        {
+            /* Write one byte to the transmit data register */
+            USART_SendData(UART5, txBuffer[txBufferOut++]);
+        }
+    }
+}
+
 // PC12 UART5_TX alternate function
 // PD2 UART5_RX alternate function
 void UART_Init(void)
@@ -12,64 +44,129 @@ void UART_Init(void)
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOD | RCC_APB2Periph_AFIO, ENABLE);
 
     // configure GPIOC.12 as push-pull
-    GPIO_InitTypeDef  gpio_init;
+    GPIO_InitTypeDef gpio_init;
     gpio_init.GPIO_Pin   = GPIO_Pin_12;
     gpio_init.GPIO_Speed = GPIO_Speed_50MHz;
     gpio_init.GPIO_Mode  = GPIO_Mode_AF_PP;
     GPIO_Init(GPIOC, &gpio_init);
 
     // and GPIOD.2 as input floating
-    gpio_init.GPIO_Pin   = GPIO_Pin_2;
-    gpio_init.GPIO_Mode  = GPIO_Mode_IN_FLOATING;
+    gpio_init.GPIO_Pin  = GPIO_Pin_2;
+    gpio_init.GPIO_Mode = GPIO_Mode_IN_FLOATING;
     GPIO_Init(GPIOD, &gpio_init);
 
     USART_InitTypeDef usart_init;
     usart_init.USART_BaudRate            = UART_DATA_RATE;
     usart_init.USART_WordLength          = USART_WordLength_8b;
     usart_init.USART_StopBits            = USART_StopBits_1;
-    usart_init.USART_Parity              = USART_Parity_No ;
+    usart_init.USART_Parity              = USART_Parity_No;
     usart_init.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-    usart_init.USART_Mode                = USART_Mode_Tx;
+    usart_init.USART_Mode                = USART_Mode_Rx | USART_Mode_Tx;
     USART_Init(UART5, &usart_init);
 
-    USART_ClockInitTypeDef usart_clk_init;
-    usart_clk_init.USART_Clock               = USART_Clock_Disable;
-    usart_clk_init.USART_CPOL                = USART_CPOL_Low;
-    usart_clk_init.USART_CPHA                = USART_CPHA_2Edge;
-    usart_clk_init.USART_LastBit             = USART_LastBit_Disable;
-    USART_ClockInit(UART5, &usart_clk_init);
+    // enable interrupts
+    USART_ITConfig(UART5, USART_IT_RXNE, ENABLE);
+    // USART_ITConfig(UART5, USART_IT_TXE, ENABLE); // TODO
 
-    USART_Cmd(UART5, ENABLE);                             /* enable UART5 */
+    USART_Cmd(UART5, ENABLE);          /* enable UART5 */
 }
 
-void UART_sendByte(uint8_t byte)
+bool UART_transmitByte(uint8_t byte)
 {
-    /* Send one byte from UART */
-    USART_SendData(UART5, byte);
-
-    /* Loop until UART DR register is empty */ 
-    while(USART_GetFlagStatus(UART5, USART_FLAG_TXE) == RESET)
+    // TODO get interrupt-driven transmit to work
+#if 0
+    bool retval = false;
+    // if the transmit buffer is empty
+    if (USART_GetFlagStatus(UART5, USART_FLAG_TXE) != RESET)
+    {
+        /* Write one byte to the transmit data register */
+        USART_SendData(UART5, byte);
+        retval = true;
+    }
+    else
+    {
+    __disable_irq();
+    if (txBufferIn < txBufferSize)
+    {
+        txBuffer[txBufferIn++] = byte;
+        retval                 = true;
+    }
+    __enable_irq();
+    }
+    return retval;
+#else
+    while (USART_GetFlagStatus(UART5, USART_FLAG_TXE) != SET)
         /* spin */;
+    USART_SendData(UART5, byte);
+    return true;
+#endif
+
 }
 
 bool UART_receiveByte(uint8_t *buffer, uint16_t timeout)
 {
+    bool done        = false;
     uint32_t endTime = systemTime + timeout;
 
-    while(USART_GetFlagStatus(UART5, USART_FLAG_RXNE) == RESET)
+    while (systemTime < endTime)
     {
-        if (systemTime > endTime)
-            return false;
+        __disable_irq();
+        if (rxBufferOut < rxBufferIn)
+        {
+            *buffer = rxBuffer[rxBufferOut++];
+            done    = true;
+        }
+        __enable_irq();
+        if (done)
+            return true;
     }
+    return false;                      // timeout
+}
 
-    *buffer = USART_ReceiveData(UART5);
-    return true;
+void UART_flushReceivedCharacters(void)
+{
+    __disable_irq();
+    rxBufferOut = rxBufferIn = 0;
+    __enable_irq();
+}
+
+// receive up to bufsize characters, or until a CR or LF character.
+// CR characters are turned into LF characters.
+// The LF character will be included as the last character of the line.
+uint8_t UART_receiveLine(uint8_t *buffer, uint8_t bufsize, uint16_t timeout)
+{
+    uint8_t nRead    = 0;
+    uint32_t endTime = systemTime + timeout;
+
+    while (systemTime < endTime)
+    {
+        uint8_t c;
+        __disable_irq();
+        while (nRead < bufsize && rxBufferOut < rxBufferIn)
+        {
+            c = rxBuffer[rxBufferOut++];
+            if (c == '\r')
+                c = '\n';
+            *buffer++ = c;
+            nRead++;
+        }
+        if ((c == '\n') || (nRead >= bufsize))
+            break;
+        else
+            __enable_irq();
+    }
+    // reset rcv indexes
+    rxBufferIn = rxBufferOut = 0;
+    __enable_irq();
+    return nRead;                      // timeout
 }
 
 void UART_printString(const char *s)
 {
     for (const char *p = s; *p; p++)
-        UART_sendByte(*p);
+    {
+        UART_transmitByte(*p);
+    }
 }
 
 int UART_printf(const char *format, ...)
--- firmware/src/UART.h
+++ firmware/src/UART.h
@@ -3,16 +3,30 @@
 
 #define UART_DATA_RATE 115200
 
-void UART_Init(void);
+extern void UART5_IRQHandler(void);
 
-// simple, polled send
-void UART_sendByte(uint8_t byte);
+extern void UART_Init(void);
 
-// simple, polled receive with timeout in system ticks
+void UART5_IRQHandler(void);
+
+// interrupt-driven transmit byte; returns false if no room
+bool UART_transmitByte(uint8_t byte);
+
+// interrupt-driven receive byte; returns false if bytes by timeout
 bool UART_receiveByte(uint8_t *buffer, uint16_t timeout);
 
+// throw away received characters
+void UART_flushReceivedCharacters(void);
+
+// receive up to bufsize characters, or until a CR or LF character.
+// CR characters are turned into LF characters.
+// The LF character will be included as the last character of the line.
+uint8_t UART_receiveLine(uint8_t *buffer, uint8_t bufsize, uint16_t timeout);
+
+// print a string to the UART
 void UART_printString(const char *s);
 
+// printf via UART
 int UART_printf(const char *format, ...);
 
 #endif
